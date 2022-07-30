bacf00f2bca67094683c2a3da45fff57a1716e6b
vagrant <bahar.hosseini@gmail.com>
2022-07-30 20:06:54 +0000

    the sumItems function adds up nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
-
+  let sumOfNumber = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      sumOfNumber += sumItems(item);
+    } else {
+      sumOfNumber += item;
+    }
+  }
+  return sumOfNumber;
+};
 module.exports = sumItems;
